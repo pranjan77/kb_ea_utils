46ae6d76bc4fb3f8bd7c7a3470cae822be1d2925
Dylan Chivian <DCChivian@lbl.gov>
2016-11-30 14:46:34 -0800

    added additional third party codes fastq-multx fastq-join and determine-phred

--- third_party/install_ea_utils.pl
+++ third_party/install_ea_utils.pl
@@ -6,6 +6,12 @@ my $cmd = "git clone https://github.com/earonesty/ea-utils\n";
    $cmd .= "make\n";
    $cmd .= "cp fastq-stats /usr/local/bin\n";
    $cmd .= "which fastq-stats\n";
+   $cmd .= "cp fastq-multx /usr/local/bin\n";
+   $cmd .= "which fastq-multx\n";
+   $cmd .= "cp fastq-join /usr/local/bin\n";
+   $cmd .= "which fastq-join\n";
+   $cmd .= "cp determine-phred /usr/local/bin\n";
+   $cmd .= "which determine-phred\n";
    system ($cmd);
 }
 
