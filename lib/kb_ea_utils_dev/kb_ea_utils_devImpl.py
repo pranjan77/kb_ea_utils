873c3b9ede0159fe9b06db68f1726b8986f058f6
Dylan Chivian <dcchivian@lbl.gov>
2016-12-08 22:24:35 -0800

    debugging

--- lib/kb_ea_utils_dev/kb_ea_utils_devImpl.py
+++ lib/kb_ea_utils_dev/kb_ea_utils_devImpl.py
@@ -611,7 +611,8 @@ class kb_ea_utils_dev:
         try:
             #p = subprocess.Popen(" ".join(multx_cmd), cwd=self.scratch, shell=False)
             #p = subprocess.Popen(" ".join(multx_cmd), cwd=self.scratch, shell=True)
-            p = subprocess.Popen(" ".join(multx_cmd), stdout=subprocess.PIPE, stderr=subprocess.STDERR, cwd=self.scratch, shell=True)
+#            p = subprocess.Popen(" ".join(multx_cmd), stdout=subprocess.PIPE, stderr=subprocess.STDERR, cwd=self.scratch, shell=True)
+            p = subprocess.Popen(" ".join(multx_cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=self.scratch, shell=True)
         except:
             raise ValueError('Error starting subprocess for fastq-multx')
 
